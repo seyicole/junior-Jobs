75cade6084937a89c2c7adb28cc4f0ebf42afed7
sey cole <seyicole>
2020-05-27 23:25:14 +0100

    Updates

---
+++ precache-manifest.2abafeeacf7b91e7c532192b262af00e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9773e594724705acc0b63d2d2593be4b",
+    "url": "/junior-Jobs/index.html"
+  },
+  {
+    "revision": "c23578b2666f9738ddcd",
+    "url": "/junior-Jobs/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "ce649205b23767fd8ead",
+    "url": "/junior-Jobs/static/css/main.62a0af41.chunk.css"
+  },
+  {
+    "revision": "c23578b2666f9738ddcd",
+    "url": "/junior-Jobs/static/js/2.5b303814.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/junior-Jobs/static/js/2.5b303814.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ce649205b23767fd8ead",
+    "url": "/junior-Jobs/static/js/main.aaec58f7.chunk.js"
+  },
+  {
+    "revision": "a91e1aa79073d209d0b2",
+    "url": "/junior-Jobs/static/js/runtime-main.1da78500.js"
+  }
+]);
